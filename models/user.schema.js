c3239b8e49a0104f91eeaf3a9979dda5345c0e69
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-01-16 13:47:17 +0530

    added userSchema & config

---
+++ models/user.schema.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
+import config from "../config/index";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      maxLength: [30, "Name must be less than 30"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be atleast 8 chars"],
+      select: false, //whenever  make query to db this field will not return to frontend
+    },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true, // adds two properties of type date to schema (createdAt & updatedAt)
+  }
+);
+
+// encrypting the password with mongoose pre-hooks(middleware)
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); //if password is not modified return to next middleware if any
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// adding more features directly to schema
+userSchema.methods = {
+  //compare password
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+
+  //   generate JWT token
+  getJwtToken: function () {
+    return JWT.sign(
+      {
+        _id: this._id,
+        role: this.role,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: config.JWT_EXPIRY,
+      }
+    );
+  },
+};
+
+export default mongoose.model("User", userSchema);
