055f88ed4f00ad11461a9760104069d48fd98bb2
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-01-17 14:51:34 +0530

    added order utils

---
+++ utils/paymentMode.js
@@ -0,0 +1,10 @@
+const PaymentMode = {
+  COD: "COD",
+  CREDITCARD: "CREDITCARD",
+  DEBITCARD: "DEBITCARD",
+  UPI: "UPI",
+  NETBANKING: "NETBANKING",
+  SHOPEWALLET: "SHOPEWALLET",
+};
+
+export default PaymentMode;
